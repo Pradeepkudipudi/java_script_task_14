32dba711ff028be0c03a7381aece1e8311d834b3
Pradeepkudipudi <pradeepk70220@gmail.com>
2024-11-22 10:19:41 +0530

    square numbers & toUppercase

---
+++ index.js
@@ -0,0 +1,23 @@
+
+// TODAY TASKS
+
+// 1.Write a function squareNumbers(arr) that returns a new array 
+//where each number in the original array is squared using map method.
+
+
+let squared =  [2,3,4,5,6,7]
+
+x = squared.map(a=>a**2)
+
+console.log(x)
+
+
+// 2.Write a function toUppercase(arr) that returns a new array 
+//where each string in the original array is converted to uppercase using map method.
+
+
+let uppercase = ["html","css","aws"];
+
+x = uppercase.map(a=>a.toUpperCase())
+console.log(x)
+
